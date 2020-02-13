b7a5bca8cf82b4a2e4c31e03ab15a03349cb66b5
jbellevi <juliebelleville@protonmail.com>
2020-02-12 23:34:29 -0800

    completed data generation code

--- Labs/Julie/lab5/image_set_generator.py
+++ Labs/Julie/lab5/image_set_generator.py
@@ -4,7 +4,7 @@ import os
 
 class ImageSetGenerator:
 
-    def __init__(self, input_path='enph353_cnn_lab/pictures/',
+    def __init__(self, input_path='enph353_cnn_lab/letter_pictures/',
                  output_path='outputs/', img_width=300):
         self.input_dir = os.path.dirname(os.path.realpath(__file__)) + \
             "/" + input_path
@@ -26,15 +26,16 @@ class ImageSetGenerator:
                   cv2.IMREAD_GRAYSCALE) for file in files]
         processed_plates = [self.process_image(plate) for plate in plates]
         letters = [None] * 4 * len(processed_plates)
+        labels = self.get_labels(files)
+
         i = 0
         for plate in processed_plates:
             a, b, c, d = self.segment_plates(plate)
-            letters[i] = a
-            letters[i + 1] = b
-            letters[i + 2] = c
-            letters[i + 3] = d
+            letters[i] = (a, labels[i] + '_0')
+            letters[i + 1] = (b, labels[i + 1] + '_1')
+            letters[i + 2] = (c, labels[i + 2] + '_0')
+            letters[i + 3] = (d, labels[i + 3] + '_1')
             i = i + 4
-
         return letters
 
     def process_image(self, img):
@@ -48,23 +49,35 @@ class ImageSetGenerator:
     def segment_plates(self, plate):
         # hardcoded for now, find more better method
         w = int(plate.shape[1]/4)
-        # cv2.imshow('hi', plate[:, 0:w])
-        # cv2.waitKey(2000)
-        # cv2.imshow('hi', plate[:, w:2*w])
-        # cv2.waitKey(2000)
-        # cv2.imshow('hi', plate[:, 2*w:3*w])
-        # cv2.waitKey(2000)
-        # cv2.imshow('hi', plate[:, 3*w:4*w])
-        # cv2.waitKey(2000)
         return plate[:, 0:w], plate[:, w:2*w], plate[:, 2*w:3*w], \
             plate[:, 3*w:4*w]
+
+    def get_labels(self, files):
+        """ relies on file name format: plate_AA11.png"""
+        labels = [None] * 4 * len(files)
+        i = 0
+        for name in files:
+            labels[i] = name[6]
+            labels[i + 1] = name[7]
+            labels[i + 2] = name[8]
+            labels[i + 3] = name[9]
+            i = i + 4
+
+        return labels
+
+    def save_plate_segment(self, plate, character):
+        cv2.imwrite("{} letter_{}.png".format(self.output_dir,
+                    character), plate)
+
+    def generate_letter_set(self):
+        letters = self.get_letters(self.files_in_folder())
+        for letter in letters:
+            self.save_plate_segment(letter[0], letter[1])
 
 
 def main():
     img_gen = ImageSetGenerator()
-    letters = img_gen.get_letters(img_gen.files_in_folder())
-    cv2.imshow('hi', letters[0])
-    cv2.waitKey(10000)
+    img_gen.generate_letter_set()
 
 
 if __name__ == "__main__":
