95c075f2d740912712da5b0eae672222ef89df07
jbellevi <juliebelleville@protonmail.com>
2020-03-05 16:36:31 -0800

    augmentation functions mostly complete, minus cropping

--- Competition/ImageGenerationAugmentation/test_augmentation.py
+++ Competition/ImageGenerationAugmentation/test_augmentation.py
@@ -17,39 +17,71 @@ def test_rotate(util):
         img = util.rotate(15 * i)
         cv2.imshow("rotate", img)
         cv2.waitKey(1000)
+    # consider: https://stackoverflow.com/questions/16702966/rotate-image-and-crop-out-black-borders
 
 
 def test_blur(util):
-    pass
+    for i in range(1, 15):
+        img = util.blur(i * 2 + 1)
+        cv2.imshow("blur", img)
+        cv2.waitKey(1000)
 
 
 def test_motion_blur(util):
-    pass
+    for i in range(1, 15):
+        img = util.motion_blur(i * 2 + 1)
+        cv2.imshow("motion_blur", img)
+        cv2.waitKey(1000)
 
 
 def test_noise(util):
-    pass
+    modes = ['gaussian', 'localvar', 'poisson', 'salt', 'pepper', 's&p', 'speckle']
+    # Notes: salt, pepper, s&p are not as useful I think
+    for mode in modes:
+        img = util.noise(mode)
+        cv2.imshow("noise", img)
+        cv2.waitKey(1000)
+
+    for i in range(10):
+        img = util.rand_noise()
+        cv2.imshow("noise", img)
+        cv2.waitKey(1000)
 
 
 def test_hsb(util):
-    pass
+    for i in range(10):
+        img = util.hsb()
+        cv2.imshow("hsb", img)
+        cv2.waitKey(1000)
 
 
 def test_shift(util):
-    pass
+    for dx in range(-2, 2):
+        for dy in range(-2, 2):
+            img = util.shift(dx * 50, dy * 50)
+            cv2.imshow("shift", img)
+            cv2.waitKey(1000)
+
+
+def test_perspective_transform(util):
+    for i in range(10):
+        img = util.perspective_transform()
+        cv2.imshow("perspective", img)
+        cv2.waitKey(1000)
 
 
 def main():
     print("exterminate: being test")
     img = cv2.imread('dalek_test.jpeg')
     aug_util = au(img)
-    test_scale(aug_util)
-    test_rotate(aug_util)
-    test_blur(aug_util)
-    test_motion_blur(aug_util)
-    test_noise(aug_util)
-    test_hsb(aug_util)
-    test_shift(aug_util)
+    # test_scale(aug_util)
+    # test_rotate(aug_util)
+    # test_blur(aug_util)
+    # test_motion_blur(aug_util)
+    # test_noise(aug_util)
+    # test_hsb(aug_util)
+    # test_shift(aug_util)
+    test_perspective_transform(aug_util)
     print("Test complete: successful extermination")
 
 
