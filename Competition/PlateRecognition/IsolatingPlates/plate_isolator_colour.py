3ace28052f194a6ba0a5ca3e4d1a775e1f1caa7d
jbellevi <juliebelleville@protonmail.com>
2020-03-15 17:45:36 -0700

    colour mask working

---
+++ Competition/PlateRecognition/IsolatingPlates/plate_isolator_colour.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+
+import cv2
+import numpy as np
+
+
+class PlateIsolatorColour:
+    """
+    The goal of this module is to pick out parking
+    1. will pull out cars by colour
+
+    Input: a clean image of the plates
+           Random, likely terrible images picked up from Anki camera
+
+    Resources:
+    https://www.pyimagesearch.com/2014/08/04/opencv-python-color-detection/
+    """
+
+    def __init__(self, colour_bounds=None):
+        """
+        Sets up our sift recognition based off of our pattern
+        """
+        # in order HSB, green, blue, yellow
+        if colour_bounds is None:
+            self.colour_bounds = [
+                ([50, 20, 20], [80, 240, 240]),
+                ([90, 20, 20], [130, 240, 240]),
+                ([0, 20, 20], [60, 255, 255])
+            ]
+        else:
+            self.colour_bounds = colour_bounds
+
+    def detectCar(self, img, duration=1000):
+        bound_num = 0
+
+        hsb = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+        for (lower, upper) in self.colour_bounds:
+            # create numpy arrays from colour boundaries
+            lower = np.array(lower, dtype="uint8")
+            upper = np.array(upper, dtype="uint8")
+
+            # find colours in the range, apply mask
+            if (bound_num == 0):
+                title = "green"
+            elif (bound_num == 1):
+                title = "blue"
+            else:
+                title = "yellow"
+            
+            bound_num += 1
+
+            mask = cv2.inRange(hsb, lower, upper)
+            output = cv2.bitwise_and(img, img, mask=mask)
+            cv2.imshow(title, np.hstack([img, output]))
+            cv2.waitKey(duration)
+
+    def undistort(self, img):
+        mtx = np.load("mtx.npy")
+        dist = np.load("dist.npy")
+
+        h, w = img.shape[:2]
+        newcameramtx, roi = cv2.getOptimalNewCameraMatrix(
+            mtx, dist, (w, h), 1, (w, h))
+
+        # undistort
+        dst = cv2.undistort(img, mtx, dist, None, newcameramtx)
+
+        # crop the image
+        x, y, w, h = roi
+        dst = dst[y:y+h, x:x+w]
+        return dst
--- Competition/PlateRecognition/IsolatingPlates/test_plate_isolator_colour.py
+++ Competition/PlateRecognition/IsolatingPlates/test_plate_isolator_colour.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
 
 import cv2
-from plate_isolator_orb import PlateIsolatorOrb as PlateIsolator
-
+from plate_isolator_colour import PlateIsolatorColour
 
 def scale_image(img):
-    MAX_IMG_WIDTH = 600
+    MAX_IMG_WIDTH = 400
 
     width = img.shape[1]
     height = img.shape[0]
@@ -17,19 +16,15 @@ def scale_image(img):
     return img
 
 
-def test_image(img_path, isolator, duration=10000):
+def test_image(img_path, isolator, duration=2000):
     detect_image = cv2.imread(img_path)
-    # detect_image = scale_image(detect_image)
-    matches = isolator.detectFeature(detect_image, detect_image,
-                                     duration=duration)
-    print(matches)
-    pass
+    detect_image = scale_image(detect_image)
+    isolator.detectCar(detect_image,
+                       duration=duration)
 
 
 def main():
-    feature_img = cv2.imread('dev_images/blue_crop_parking_only.jpg')
-    isolator = PlateIsolator(feature_img)
-    isolator.show_ref_and_keypoints()
+    isolator = PlateIsolatorColour()
 
     test_image('dev_images/test0_nobg.jpg', isolator)
     test_image('dev_images/test1_nobg.jpg', isolator)
@@ -43,7 +38,6 @@ def main():
     # test_image('dev_images/blue_crop_noletters.jpg', isolator)
     # test_image('dev_images/green_crop.jpg', isolator)
     # test_image('dev_images/yellow_crop.jpg', isolator)
-
 
     print("end of main")
 
